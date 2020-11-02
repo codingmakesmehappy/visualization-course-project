74b22ebcafee7306b063cfa6b955ee39be48b94b
dcalsky <dcalsky@163.com>
2020-11-02 15:58:45 +0800

    feat: add embed chart and echart

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import "./index.css";
+import echarts from "echarts";
 
 class App {
   constructor() {
     console.log("Greeting!");
+    this.initChart1();
+  }
+  initChart1() {
+    var myChart = echarts.init(
+      document.getElementById("chart1") as HTMLDivElement,
+    );
+    myChart.setOption({
+      title: {
+        text: "ECharts entry example",
+      },
+      tooltip: {},
+      xAxis: {
+        data: ["shirt", "cardign", "chiffon shirt", "pants", "heels", "socks"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "sales",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    });
   }
 }
 
