5c04f99bc329af458ce943edcfbc2f23508323e6
dcalsky <dcalsky@163.com>
2020-11-11 19:41:01 +0800

    feat: apply reveal.js

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import "./index.css";
 import echarts from "echarts";
+import Reveal from "reveal.js";
+// import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import "reveal.js/dist/reveal.css";
+import "reveal.js/dist/theme/serif.css";
 
 class App {
   constructor() {
     console.log("Greeting!");
-    this.initChart1();
+    // this.initChart1();
+    let deck = new Reveal({
+      // plugins: [Markdown],
+    });
+    deck.initialize({
+      width: 1440,
+      height: 960,
+    });
   }
+
   initChart1() {
     var myChart = echarts.init(
       document.getElementById("chart1") as HTMLDivElement,
