0c6ae00096b767b94b6717361d92c5cc56b2270c
dcalsky <dcalsky@163.com>
2020-11-14 17:27:09 +0800

    feat: append heat maps

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import "./index.css";
+
 import echarts from "echarts";
 import Reveal from "reveal.js";
 // import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
 import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/serif.css";
+import "./index.css";
 
 class App {
   constructor() {
