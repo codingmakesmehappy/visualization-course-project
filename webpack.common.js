c5e0daf8454f54209106aa0811619cab96895ed8
dcalsky <dcalsky@163.com>
2020-11-23 00:25:00 +0800

    chore: add html-loader to support load medias in html file

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,14 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 module.exports = {
   entry: "./src/main.ts",
   resolve: {
     extensions: [".ts", ".js", ".json"],
+    alias: {
+      "@assets": path.resolve("./src/assets"),
+    },
   },
   module: {
     rules: [
@@ -14,6 +18,10 @@ module.exports = {
         use: {
           loader: "babel-loader",
         },
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
       },
       {
         test: /\.css$/,
@@ -31,7 +39,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpe?g|gif|woff(2)?|ttf|eot|svg)$/i,
+        test: /\.(png|jpe?g|gif|woff(2)?|ttf|eot|svg|mp4)$/i,
         use: [
           {
             loader: "file-loader",
